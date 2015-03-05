e90b42f4a89a6abec4aced6f4b9357ea4796a204
Alex Land <aland@sfu.ca>
2015-03-05 12:01:13 -0800

    Uncommented code that was causing problems

--- app/src/main/java/com/cmpt276/meetly/Parent.java
+++ app/src/main/java/com/cmpt276/meetly/Parent.java
@@ -20,14 +20,14 @@ public class Parent extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_parent);
+        setContentView(R.layout.activity_parent);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_parent, menu);
+        getMenuInflater().inflate(R.menu.menu_parent, menu);
         return true;
     }
 
