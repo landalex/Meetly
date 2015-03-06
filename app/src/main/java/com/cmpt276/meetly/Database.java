18741d823d28eb9d469f95df4963c095cb08cf40
Hami Tusiime <htusiime@sfu.ca>
2015-03-06 14:34:33 -0800

    added overloading constructor, copy constructor, and accessors and mutators for event details

--- app/src/main/java/com/cmpt276/meetly/Database.java
+++ app/src/main/java/com/cmpt276/meetly/Database.java
@@ -1,39 +1,7 @@
 package com.cmpt276.meetly;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class Database extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_database);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_database, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+/**
+ * Created by Hami on 3/6/2015.
+ */
+public class Database {
 }
--- app/src/main/java/com/cmpt276/meetly/Event.java
+++ app/src/main/java/com/cmpt276/meetly/Event.java
@@ -6,19 +6,86 @@ import java.util.Date;
  * Created by AlexLand on 15-03-05.
  */
 public class Event {
+    private int _id;
     private String title;
     private String date;
+    private String location;
+    //attendees limit defaults to 25
+    private String[] attendees = new String[25];
+    private String notes;
 
+
+    /**
+     * Event Constructor
+     * @param title
+     * @param date
+     * @param location
+     * @param notes
+     */
+    public Event(String title, Date date, String location, String notes) {
+        this.title = title;
+        this.date = date.toString();
+        this.location = location;
+        this.notes = notes;
+    }
+
+    /**
+     * Event Constructor
+     * @param title
+     * @param date
+     * @param location
+     * @param notes
+     */
+    public Event(String title, Date date, String location, String[] attendees, String notes) {
+        this.title = title;
+        this.date = date.toString();
+        this.location = location;
+        this.attendees = attendees;
+        this.notes = notes;
+    }
+
+    /**
+     * Event Copy Constructor
+     * @param eventCopy : The event to copy
+     */
+    public Event(Event eventCopy) {
+        this.title = eventCopy.getTitle();
+        this.date = eventCopy.getTitle();
+        this.location = eventCopy.getLocation();
+        this.attendees = eventCopy.getAttendees();
+        this.notes = eventCopy.getNotes();
+    }
+
+    /**
+     * Event Constructor (created by Alex)
+     * @param title
+     * @param date
+     */
     public Event(String title, Date date) {
         this.title = title;
         this.date = date.toString();
     }
 
-    public String getTitle() {
-        return title;
-    }
 
-    public String getDate() {
-        return date;
-    }
+    // Accessors
+    public String getTitle() {return title;}
+
+    public String getDate() {return date;}
+
+    public String getLocation() {return location;}
+
+    public String[] getAttendees() {return attendees;}
+
+    public String getNotes(){ return notes;}
+
+    //Basic Mutators
+    public void setTitle(String newTitle) {title = newTitle;}
+
+    public void setDate(Date newDate) {date = newDate.toString();}
+
+    public void setLocation(String newLocation) {location = newLocation;}
+
+    public void setAttendees(String[] newAttendees) {attendees = newAttendees;}
+
+    public void setNotes(String newNotes){ notes = newNotes;}
 }
