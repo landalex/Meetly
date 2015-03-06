080fb11438334381b4253eac8054aa304c7ec62b
Hami Tusiime <htusiime@sfu.ca>
2015-03-05 23:38:45 -0800

    added database columns for app

--- app/src/main/java/com/cmpt276/meetly/MySQLiteHelper.java
+++ app/src/main/java/com/cmpt276/meetly/MySQLiteHelper.java
@@ -10,18 +10,27 @@ import android.util.Log;
  */
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-    public static final String TABLE_COMMENTS = "comments";
+    public static final String TABLE_EVENTS = "events";
     public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_COMMENT = "comment";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_DATE = "date";
+    public static final String COLUMN_LOCATION = "location";
+    public static final String COLUMN_ATTENDEES= "attendees";
+    public static final String COLUMN_NOTES = "notes";
 
     private static final String DATABASE_NAME = "MeetlyDB";
     private static final int DATABASE_VERSION = 1;
 
     // Database creation sql statement
     private static final String DATABASE_CREATE = "create table "
-            + TABLE_COMMENTS + "(" + COLUMN_ID
-            + " integer primary key autoincrement, " + COLUMN_COMMENT
-            + " text not null);";
+            + TABLE_EVENTS + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_TITLE + " text not null,"
+            + COLUMN_DATE + "char(19),"
+            + COLUMN_LOCATION + "text,"
+            + COLUMN_ATTENDEES + "text,"
+            + COLUMN_NOTES + "char(1000)"
+            + ");";
 
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -37,7 +46,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
         Log.w(MySQLiteHelper.class.getName(),
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENTS);
         onCreate(db);
     }
 
