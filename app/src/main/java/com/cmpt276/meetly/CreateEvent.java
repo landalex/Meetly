d60c68119643e3b33d66486a1892e25918c7475f
Jasdeep Jassal <jjassal@sfu.ca>
2015-03-12 01:21:44 -0700

    Map integrated onto create event page.

--- app/src/main/java/com/cmpt276/meetly/CreateEvent.java
+++ app/src/main/java/com/cmpt276/meetly/CreateEvent.java
@@ -2,28 +2,55 @@ package com.cmpt276.meetly;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.FragmentTransaction;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Fields: Name, Location (Open map), Date/Time (Use the circle thingy), People (Open contacts), Notes (1000 characters?)
  */
-public class CreateEvent extends Activity {
+public class CreateEvent extends Activity { //extends FragmentActivity implements OnMapReadyCallback {
 
     private final String TAG = "CreateEventActivity";
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
+//        chooseLocationButton();
         chooseTimeButton();
         chooseDateButton();
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+
     }
+
+//    private void chooseLocationButton() {
+//        Button chooseLocationBtn = (Button) findViewById(R.id.chooseLocationOnMapBtn);
+//
+//        chooseLocationBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(CreateEvent.this, MapLocationSelect.class));
+//            }
+//        });
+//    }
 
     private void chooseTimeButton() {
         final Button timeBtn = (Button) findViewById(R.id.chooseTime);
@@ -62,19 +89,28 @@ public class CreateEvent extends Activity {
 
     private void chooseDateButton() {
         final Button dateBtn = (Button) findViewById(R.id.chooseDate);
+        final int INITIAL_YEAR = 2015;
+        final int INITIAL_MONTH = 0;
+        final int INITIAL_DAY = 1;
 
         dateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                // When the buttons pressed we pop up a new DatePicker Dialog
+                // Parameters: CreateEvent.this : Tells it which Context (Activity) it is in
+                //             new DatePickerDialog.OnDateSetListener() : What to do when user clicks done
+                //             INITIAL_* : What year, month and day to show when it initially pops up
+
                 new DatePickerDialog(CreateEvent.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        dateBtn.setText("" + monthOfYear + "/" + dayOfMonth + "/" + year);
+                        dateBtn.setText(monthOfYear + 1 + "/" + dayOfMonth + "/" + year);       // Months are 0-indexed, Days are 1-indexed
                     }
-                }, 2015, 2, 1).show();
+                }, INITIAL_YEAR, INITIAL_MONTH, INITIAL_DAY).show();
             }
         });
     }
+
 
 }
