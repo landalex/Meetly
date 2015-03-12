0aef578a4fd270cdd8e313f2ebdd6b07e437adbb
Jasdeep Jassal <jjassal@sfu.ca>
2015-03-12 01:36:57 -0700

    Create Event page now has a functional map.

--- app/src/main/java/com/cmpt276/meetly/CreateEvent.java
+++ app/src/main/java/com/cmpt276/meetly/CreateEvent.java
@@ -5,19 +5,27 @@ import android.app.DatePickerDialog;
 import android.app.FragmentTransaction;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 /**
  * Fields: Name, Location (Open map), Date/Time (Use the circle thingy), People (Open contacts), Notes (1000 characters?)
@@ -32,25 +40,53 @@ public class CreateEvent extends Activity { //extends FragmentActivity implement
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
-//        chooseLocationButton();
         chooseTimeButton();
         chooseDateButton();
 
+        // Map
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        map.setMyLocationEnabled(true);
 
+
+        // Getting the users location and moving our camera there:
+
+        // Instantiate a LocationManager.
+        LocationManager locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+
+        // Criteria specifies the 'criteria' of how granular the location is
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider. AKA Returns the name of the provider that best meets the given criteria.
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Get Current Location
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        // Store current location as a Latitude and Longitude
+        LatLng myLatLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+        // Zoom the view into the users location
+        CameraUpdate myLocationCamera = CameraUpdateFactory.newLatLngZoom(myLatLng, 12);
+        map.animateCamera(myLocationCamera);
+
+
+        // Setting a marker to the user selected location
+        final ArrayList<LatLng> markerLocation = new ArrayList<>();     // Work around to having final variables in an inner anon class
+        markerLocation.add(0, myLatLng);
+
+        // Making this final to use inside the onMapClick inner class below
+        final GoogleMap finalMap = map;
+
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                finalMap.clear();
+                Marker eventMarker = finalMap.addMarker(new MarkerOptions().position(new LatLng(latLng.latitude, latLng.longitude)).title("Event"));
+                markerLocation.add(0, eventMarker.getPosition());
+            }
+        });
 
     }
-
-//    private void chooseLocationButton() {
-//        Button chooseLocationBtn = (Button) findViewById(R.id.chooseLocationOnMapBtn);
-//
-//        chooseLocationBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(CreateEvent.this, MapLocationSelect.class));
-//            }
-//        });
-//    }
 
     private void chooseTimeButton() {
         final Button timeBtn = (Button) findViewById(R.id.chooseTime);
