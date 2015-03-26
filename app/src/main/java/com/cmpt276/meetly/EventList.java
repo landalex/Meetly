2ea5e269b1cba44e5ac88221baa6355f14613200
Tina Tran <tta39@sfu.ca>
2015-03-25 18:08:20 -0700

    French is now supported as an additional language

--- app/src/main/java/com/cmpt276/meetly/EventList.java
+++ app/src/main/java/com/cmpt276/meetly/EventList.java
@@ -227,7 +227,7 @@ public class EventList extends Fragment {
     }
 
     private String getLocation() {
-        String location = "Not available";
+        String location = getString(R.string.no_location_found);
 
         LocationManager locationManager = (LocationManager) getActivity().getSystemService(getActivity().LOCATION_SERVICE);
 
