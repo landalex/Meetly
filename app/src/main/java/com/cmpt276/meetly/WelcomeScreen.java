a460b433a8deeca5c7dabf0ec6ce70dca03dd312
Tina Tran <tta39@sfu.ca>
2015-03-27 11:48:18 -0700

    App should be fully in French now

--- app/src/main/java/com/cmpt276/meetly/WelcomeScreen.java
+++ app/src/main/java/com/cmpt276/meetly/WelcomeScreen.java
@@ -13,7 +13,6 @@ import android.widget.Button;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
-import android.app.ActionBar;
 
 /**
  * Displays the start animation and logo
@@ -54,28 +53,6 @@ public class WelcomeScreen extends ActionBarActivity implements Animation.Animat
         animation.setStartOffset(delay);
         text.setVisibility(View.VISIBLE);
         text.startAnimation(animation);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
